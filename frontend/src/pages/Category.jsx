aa0467eaf1be16b0e8f2061cf26a18330848b159
Anshi <anshikaa207@gmail.com>
2024-07-19 00:00:15 +0530

    sorting by price alse done

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -18,6 +18,7 @@ const Category = () => {
   })
   const [selectCategory, setSelectCategory] = useState(urlObject);
   const [filterList,setFilterList] = useState([]);
+  const [sortBy,setSortBy] = useState("");
   const fetchData = async()=>{
     setLoading(true);
     const response = await fetch(summaryApi.filter.url,
@@ -63,6 +64,15 @@ const Category = () => {
   useEffect(()=>{
     fetchData();
   },[filterList])
+  const handleChange = (e)=>{
+    const {value} = e.target;
+    setSortBy(value);
+    if(value === "asc")
+      setData(prev=> prev.sort((a,b)=>a.sellingPrice - b.sellingPrice))
+    if(value === "dsc")
+      setData(prev=> prev.sort((a,b)=>b.sellingPrice - a.sellingPrice))
+  }
+  useEffect(()=>{},[sortBy])
   return (
     <div className="conatiner mx-auto p-4">
       <div className="hidden lg:grid grid-cols-[200px,1px]">
@@ -71,11 +81,11 @@ const Category = () => {
             <h3 className="text-lg uppercase font-medium text-slate-300 border-b pb-1 border-slate-300">Sort by </h3>
             <form action="" className="text-sm flex flex-col gap-2 py-2">
               <div className="flex items-center gap-3">
-                <input type="radio" name="sortBy" id="" />
+                <input type="radio" name="sortBy" id="" value={"asc"} checked={sortBy === "asc"} onChange={handleChange}/>
                 <label htmlFor="">Price - Low to High</label>
               </div>
               <div className="flex items-center gap-3">
-                <input type="radio" name="sortBy" id="" />
+                <input type="radio" name="sortBy" id="" value={"dsc"} checked={sortBy === "dsc"} onChange={handleChange}/>
                 <label htmlFor="">Price - High to Low</label>
               </div>
             </form>
